1fd60b9b0ff782ba80c4b4d1944d937c884faf2e
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-03-23 22:34:58 +0530

    add necessary functions for transition effect

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', function(e) {
-  console.log(e.keyCode);
+function playSound(e) {
   const audioElement = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audioElement);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audioElement) return;
   audioElement.currentTime = 0;
   audioElement.play();
-});
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
