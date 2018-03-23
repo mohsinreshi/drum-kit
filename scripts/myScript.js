a96356c4e9c9355d276f9ee4f1fad30351f920d0
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-03-23 22:12:43 +0530

    create initial function

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', function(e) {
-  console.log(e);
+  console.log(e.keyCode);
+  const audioElement = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audioElement);
+  if(!audioElement) return;
+  audioElement.currentTime = 0;
+  audioElement.play();
 });
